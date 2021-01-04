197c7d640455ce04a28f8a93d28a99f84801c465
Debasish <imdebasishpatnaik@gmail.com>
2021-01-05 01:41:14 +0530

    fix: sets default timezone to IST

--- bot.py
+++ bot.py
@@ -16,10 +16,10 @@ def get_latest_bitcoin_price():
     return {'inr': btc_price_in_inr, 'usd': btc_price_in_usd}
 
 
+istTimeDelta = datetime.timedelta(hours=5, minutes=30)
+istTZObject = datetime.timezone(istTimeDelta, name="IST")
+curr_time = datetime.datetime.now(tz=istTZObject).strftime('%d-%b-%Y %I:%M %p')
+
 curr_prices = get_latest_bitcoin_price()
-
-curr_time = datetime.datetime.now().strftime('%d-%b-%Y %I:%M %p')
-
-
 print(requests.post(IFTTT_URL + os.getenv('ifttt_api_key'),
                     data={'value1': curr_time, 'value2': curr_prices['inr'], 'value3': curr_prices['usd']}).content.decode('ascii'))
